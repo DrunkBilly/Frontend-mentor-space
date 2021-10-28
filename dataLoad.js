c1bdcf61708a0b7a0ef5a9b338c26753e2f01e6c
DrunkBilly <shlomi680@gmail.com>
2021-10-28 21:41:34 +0300

    init

---
+++ dataLoad.js
@@ -0,0 +1,72 @@
+let siteData;
+let destinations;
+let crew;
+let technology;
+
+//set selectors for planet
+const planetBody     = document.querySelector('.destination-planet-body');
+const planetTitle    = document.querySelector('.destination-title');
+const planetInfo     = document.querySelector('.destination-info');
+const planetDistance = document.querySelector('.destination-distance');
+const planetTime     = document.querySelector('.destination-time');
+
+//set selectors for crew
+const crewJob         = document.querySelector('.crew-job');
+const crewName        = document.querySelector('.crew-name');
+const crewDescription = document.querySelector('.crew-description');
+const crewPhoto       = document.querySelector('.crew-photo');
+
+//set selectors for technology
+const technologyName = document.querySelector('.technology-name');
+const technologyDescription = document.querySelector('.technology-info-text');
+const technologyPhoto = document.querySelector('.technology-photo');
+
+fetch('data.json')
+    .then(response =>{
+    return response.json();
+})
+    .then(data => {
+        siteData = data;
+        destinations = data['destinations'];
+        crew = data['crew'];
+        technology = data['technology'];
+
+        changeDestination(0);
+        changeCrew(0);
+        changeTechnology(0);
+
+        const navScript = document.createElement('script');
+        navScript.src = 'nav.js';
+        document.head.appendChild(navScript);
+    });
+
+    //remove class on list
+function removeClass(nodeList , classToRemove){
+    for(let i = 0; i < nodeList.length; i++){
+        if(nodeList[i].classList.contains(classToRemove))
+        {
+            nodeList[i].classList.remove(classToRemove);
+        }
+    }
+}
+    //change destination planet
+function changeDestination(destinationNumber){
+    planetBody.style.backgroundImage = `url('${destinations[destinationNumber].images.png} ')`;
+    planetTitle.innerHTML            = destinations[destinationNumber].name;
+    planetInfo.innerHTML             = destinations[destinationNumber].description;
+    planetDistance.innerHTML         = destinations[destinationNumber].distance;
+    planetTime.innerHTML             = destinations[destinationNumber].travel;
+}
+    //change crew member
+function changeCrew(crewNumber){
+    crewJob.innerHTML               = crew[crewNumber].role;
+    crewName.innerHTML              = crew[crewNumber].name;
+    crewDescription.innerHTML       = crew[crewNumber].bio;
+    crewPhoto.style.backgroundImage = `url('${crew[crewNumber].images.png}')`
+}
+    //change technology
+function changeTechnology(technologyNumber){
+    technologyName.innerHTML = technology[technologyNumber].name;
+    technologyDescription.innerHTML = technology[technologyNumber].description;
+    technologyPhoto.style.backgroundImage = `url('${technology[technologyNumber].images.portrait}')`
+}
---
+++ nav.js
@@ -0,0 +1,116 @@
+//Main navigation selectors
+const btnHome            = document.getElementById("home");
+const btnDestination     = document.getElementById("destination");
+const btnExplore         = document.querySelector(".body-main-explore");
+const btnCrew            = document.getElementById("crew");
+const btnTechnology      = document.getElementById("technology");
+
+const body               = document.querySelector(".body-main-info");
+const navItems           = document.getElementsByClassName('nav-item');
+
+//Planet navigation selectors
+const btnMoon            = document.getElementById("moon");
+const btnMars            = document.getElementById("mars");
+const btnEuropa          = document.getElementById("europa");
+const btnTitan           = document.getElementById("titan");
+
+const planetNavItems     = document.getElementsByClassName("destination-planet-item");
+
+//Crew navigation selectors
+const crewNavItems       = document.getElementsByClassName("crew-nav-item");
+
+//Technology navigation selectors
+const technologyNavItems = document.getElementsByClassName("technology-nav-item");
+
+//Main navigation
+btnHome.addEventListener('click', () => { //Home
+    body.style.marginLeft = "0vw";
+    removeClass(navItems, 'nav-item-selected');
+    btnHome.classList.add('nav-item-selected');
+})
+
+btnDestination.addEventListener('click', () => { //Destinations
+    body.style.marginLeft = "-100vw";
+    removeClass(navItems, 'nav-item-selected');
+    btnDestination.classList.add('nav-item-selected');
+})
+
+btnExplore.addEventListener('click', () => { //Destinations
+    body.style.marginLeft = "-100vw";
+    removeClass(navItems, 'nav-item-selected');
+    btnDestination.classList.add('nav-item-selected');
+})
+
+btnCrew.addEventListener('click', () =>{ //Crew
+    body.style.marginLeft = "-200vw";
+    removeClass(navItems, 'nav-item-selected');
+    btnCrew.classList.add('nav-item-selected');
+})
+
+btnTechnology.addEventListener('click', () =>{ //Crew
+    body.style.marginLeft = "-300vw";
+    removeClass(navItems, 'nav-item-selected');
+    btnTechnology.classList.add('nav-item-selected');
+})
+
+//Planet navigation
+
+btnMoon.addEventListener('click', () => { //Moon
+    changeDestination(0);
+    removeClass(planetNavItems, 'nav-item-selected');
+    btnMoon.classList.add('nav-item-selected');
+});
+btnMars.addEventListener('click', () => { //Mars
+    changeDestination(1);
+    removeClass(planetNavItems, 'nav-item-selected');
+    btnMars.classList.add('nav-item-selected');
+});
+btnEuropa.addEventListener('click', () => { //Europa
+    changeDestination(2);
+    removeClass(planetNavItems, 'nav-item-selected');
+    btnEuropa.classList.add('nav-item-selected');
+});
+btnTitan.addEventListener('click', () => { //Titan
+    changeDestination(3);
+    removeClass(planetNavItems, 'nav-item-selected');
+    btnTitan.classList.add('nav-item-selected');
+});
+
+//Crew navigation
+crewNavItems[0].addEventListener('click', () =>{
+    changeCrew(0);
+    removeClass(crewNavItems, 'crew-nav-item-selected');
+    crewNavItems[0].classList.add('crew-nav-item-selected');
+});
+crewNavItems[1].addEventListener('click', () =>{
+    changeCrew(1);
+    removeClass(crewNavItems, 'crew-nav-item-selected');
+    crewNavItems[1].classList.add('crew-nav-item-selected');
+});
+crewNavItems[2].addEventListener('click', () =>{
+    changeCrew(2);
+    removeClass(crewNavItems, 'crew-nav-item-selected');
+    crewNavItems[2].classList.add('crew-nav-item-selected');
+});
+crewNavItems[3].addEventListener('click', () =>{
+    changeCrew(3);
+    removeClass(crewNavItems, 'crew-nav-item-selected');
+    crewNavItems[3].classList.add('crew-nav-item-selected');
+});
+
+//Technology navigation
+technologyNavItems[0].addEventListener('click', () =>{
+    changeTechnology(0);
+    removeClass(technologyNavItems, 'technology-nav-item-selected');
+    technologyNavItems[0].classList.add('technology-nav-item-selected');
+});
+technologyNavItems[1].addEventListener('click', () =>{
+    changeTechnology(1);
+    removeClass(technologyNavItems, 'technology-nav-item-selected');
+    technologyNavItems[1].classList.add('technology-nav-item-selected');
+});
+technologyNavItems[2].addEventListener('click', () =>{
+    changeTechnology(2);
+    removeClass(technologyNavItems, 'technology-nav-item-selected');
+    technologyNavItems[2].classList.add('technology-nav-item-selected');
+});
